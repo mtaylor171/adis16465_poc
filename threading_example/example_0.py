368d16324e609e16fdfd4c2e1ed982ade927a8a9
Matt <mtaylor@nuroai>
2022-04-05 12:31:32 -0700

    thread

--- threading_example/example_0.py
+++ threading_example/example_0.py
@@ -5,11 +5,13 @@ import threading
 def spi_coms():
 	for i in range(0,4):
 		print("thread1: ", i)
+		time.sleep(0.5)
 
 
 def data_handle():
 	for i in range(0,4):
 		print("thread2: ", i)
+		time.sleep(0.5)
 
 
 t1 = threading.Thread(target=spi_coms)
