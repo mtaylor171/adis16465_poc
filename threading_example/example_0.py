59017913ff564c53290dcbbec8a602890949af5f
Matt <mtaylor@nuroai>
2022-04-05 13:54:11 -0700

    changes

--- threading_example/example_0.py
+++ threading_example/example_0.py
@@ -9,39 +9,34 @@ import numpy as np
 
 event = Event()
 
-def spi_coms(DR, glob_var):
-	#global DR
-	#global glob_var
+def data_handle():
 	while(True):
-		print(glob_var)
-		if(DR ==1):
-			print("Data received: ", glob_var)
-		if event.is_set():
-			break
+		DR = 0
+		for i in range(0,4):
+			print(i)
+			time.sleep(0.5)
+			if(i % 3 == 0):
+				glob_var = i
+				DR = 1
+
 
 DR = 0
 glob_var = 0
 
-t1 = Thread(target=spi_coms, args = (DR, glob_var, ))
+t1 = Thread(target=data_handle, args = (DR, glob_var, ))
 t1.start()
 
-def data_handle():
-	global DR
-	global glob_var
+def spi_coms():
 	while(True):
 		try:
-			DR = 0
-			for i in range(0,4):
-				print(i)
-				time.sleep(0.5)
-				if(i % 3 == 0):
-					glob_var = i
-					DR = 1
-			break
+			if(DR ==1):
+				print("Data received: ", glob_var)
+			if event.is_set():
+				break
 		except KeyboardInterrupt:
 			event.set()
 			break
 
-data_handle()
+spi_coms()
 
 t1.join()
