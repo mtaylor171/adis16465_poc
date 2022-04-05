85983b92dc6d4106facd6d6bb94274fbbdeaaa24
Matt <mtaylor@nuroai>
2022-04-05 12:39:42 -0700

    changes

--- threading_example/example_0.py
+++ threading_example/example_0.py
@@ -2,16 +2,26 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
+
+DR = 0
+glob_var = 0
+
 def spi_coms():
-	for i in range(0,4):
-		print("thread1: ", i)
-		time.sleep(0.5)
+	while(DR != 1)
+	print("Data received: ", glob_var)
 
 
 def data_handle():
-	for i in range(0,4):
-		print("thread2: ", i)
+	for i in range(0,27):
+		#print("thread2: ", i)
 		time.sleep(0.5)
+		if i % 3 == 0:
+			glob_var = i
+			DR = 1
+		DR = 0
+
+
+
 
 
 t1 = threading.Thread(target=spi_coms)
