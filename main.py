32d77efade96801126f33d4345946be9c52262fc
Matt <mtaylor@nuroai>
2022-04-11 17:07:12 -0700

    updates

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ if __name__ == "__main__":
 			data_int = int(data_str, 16)
 			data_sent = [hex(data_int >> 8), hex(data_int & 0xFF)]
 			print("Sending: ", data_sent)
-			spi.writebytes([(data_int >> 8), (data_int & 0xFF)])
+			print(spi.xfer([(data_int >> 8), (data_int & 0xFF)]))
 			time.sleep(0.5)
 			s_num = spi_read_reg(4)
 			print("*SPI TEST* Serial Number: ", BytesToHex(s_num))
