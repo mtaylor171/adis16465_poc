2661267e7c015e2d7e30557db093dbde570157bf
Matt <mtaylor@nuroai>
2022-04-11 17:13:09 -0700

    updates

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def BytesToHex(Bytes):
 
 def adis16465_setup():
 	spi.open(0, 0)
-	spi.max_speed_hz = 1000000  # Max spi speed 1MHz
+	spi.max_speed_hz = 100000  # Max spi speed 1MHz
 	spi.mode = 0b11     # spi mode 3 (CPOL = 1, CPHA = 1)
 	spi.lsbfirst = False
 	time.sleep(.5)  # give everything time to start up
